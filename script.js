bd8d82d8623fc5716f62930245ce23a784662e11
Rohan-Rajak07 <rohanrajak132@gmail.com>
2024-03-30 20:51:12 +0530

    page3

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 })
+var leave=document.querySelector(".elemsec")
+leave.addEventListener("mouseleave",()=>{
+    fix.style.display="none";
+    fix.innerHTML="";
+})
+
+var arr=[
+    '<video autoplay loop muted src="assets/id0.mp4"></video>',
+    '<video autoplay loop muted src="assets/id1.mp4"></video>',
+    '<video autoplay loop muted src="assets/id2.mp4"></video>',
+    '<video autoplay loop muted src="assets/video.mp4"></video>',
+    '<img src="assets/id4.webp">',
+    '<img src="assets/id5.webp">'
+
+];
+var fix=document.querySelector(".fiximg");
+var elem=document.querySelectorAll(".elem");
+
+elem.forEach((e)=>{
+    e.addEventListener("mouseenter",()=>{
+        var get=e.getAttribute("id");
+        fix.innerHTML=arr[get];
+        fix.style.display="block";
+        console.log(arr[get]);
+    })
+})
